35f9b03372085c1a9ad8509cfda8ac7635eddb79
Zara akbar <164236263+Zaraakbar28@users.noreply.github.com>
2024-05-04 03:57:08 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todo = [];
+let codition = true;
+while (codition) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "todo",
+            type: 'input',
+            message: "what you want to do add in your Todos?"
+        },
+        { name: 'addmore',
+            type: 'confirm',
+            message: "Do you want to add more?",
+            default: "false",
+        }
+    ]);
+    todo.push(addTask.todo);
+    codition = addTask.addmore;
+    console.log(todo);
+}
